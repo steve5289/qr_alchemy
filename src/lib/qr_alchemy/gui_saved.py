9e2e2a47e4668939fcf0bf00db6af61379f99ce2
steve <steve5289@users.noreply.github.com>
2021-09-01 16:45:22 -0600

    got both save and history screens setup

--- src/lib/qr_alchemy/gui_saved.py
+++ src/lib/qr_alchemy/gui_saved.py
@@ -9,18 +9,66 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class QrSavedWindow(Gtk.Window):
+    # HACK ALERT!!!!
+    # These variables are for stopping this program from automatically launching 
+    # the first thing shown in the list (due ti it getting 'selected'
+    # These must be a better way, but I can't find one right now...
     first_select=0
+    first_select_hist=0
+    first_select_sav=0
     def __init__(self):
         super().__init__(title="QR Alchemy")
         self.set_border_width(0)
 
-
-        self.add(self.page_history())
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+        self.notebook.append_page(self.page_saved(), Gtk.Label('Saved'))
+        self.notebook.append_page(self.page_history(), Gtk.Label('History'))
 
 
         self.show_all()
 
 
+    def page_saved(self):
+        saved=qr_saved.get_saved_codes()
+
+        box = Gtk.Box()
+        # Creating the ListStore model
+        ls_saved = Gtk.ListStore(str, str)
+        for name in sorted(saved.keys()):
+            print('things', name, saved[name])
+            ls_saved.append([name, saved[name]])
+
+        tv_saved = Gtk.TreeView(model=ls_saved)
+        for i, column_title in enumerate(
+            ["Name", "QR Code"]
+        ):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column_title, renderer, text=i)
+            tv_saved.append_column(column)
+            if column_title == "QR Code":
+                column.set_resizable(True)
+                column.set_max_width(50)
+        select = tv_saved.get_selection()
+        #select.set_mode(Gtk.SelectionMode.SINGLE)
+        tv_saved.connect('cursor-changed', self.selected_saved_entry)
+
+        # setting up the layout, putting the treeview in a scrollwindow, and the buttons in a row
+        stv_saved = Gtk.ScrolledWindow()
+        stv_saved.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        box.pack_start(stv_saved, True, True, 1)
+        stv_saved.add(tv_saved)
+        return box
+
+    def selected_saved_entry(self, tv_saved):
+        if self.first_select_sav == 1 or (self.first_select_hist == 0 and self.first_select_sav == 0):
+            #model, treeitr = selection.get_selected()
+            selected = tv_saved.get_selection()
+            data, i = selected.get_selected()
+            if i is not None:
+                qr_code=data[i][1]
+                gui_process.qr_gui_handle_code(qr_code)
+        self.first_select_sav=1
 
     def page_history(self):
         history=qr_saved.get_history()
@@ -53,14 +101,14 @@ class QrSavedWindow(Gtk.Window):
         return box
 
     def selected_hist_entry(self, tv_hist):
-        if self.first_select == 1:
+        if self.first_select_hist == 1 or (self.first_select_hist == 0 and self.first_select_sav == 0):
             #model, treeitr = selection.get_selected()
             selected = tv_hist.get_selection()
             data, i = selected.get_selected()
             if i is not None:
                 qr_code=data[i][1]
                 gui_process.qr_gui_handle_code(qr_code)
-        self.first_select=1
+        self.first_select_hist=1
 
         
         
