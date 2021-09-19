d620360fd3042ea49a3c439ea62b83e0573e9abd
steve <steve5289@users.noreply.github.com>
2021-09-19 15:25:50 -0600

    Pulled history page out to a seperate lib for better readability and organization

---
+++ src/lib/qr_alchemy/gui_hist.py
@@ -0,0 +1,83 @@
+
+import gi
+import qr_alchemy.process as qr_process
+import qr_alchemy.saved as qr_saved
+import qr_alchemy.gui as gui
+import qr_alchemy.gui_process as gui_process
+import qr_alchemy.gui_config as gui_config
+import qr_alchemy.gui_saved as gui_saved
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk,Gio
+
+import traceback
+
+class QrHistPage(Gtk.Window):
+    # HACK ALERT!!!!
+    # This uses the select multiple entries for the treeviews. This is 
+    # because this select option doesn't try to auto select things when 
+    # the tabs are switched.
+    # Currently picking an entry is handled by adding a function to the 
+    # selection that always says don't select. This is done as it prevents 
+    # selection but also allows us to call things when there is a selection.
+    # 
+    # There must be a better way of doing this... But I haven't found it yet...
+    hist_codes=list()
+    disable_actions=False
+    box = Gtk.Box()
+    def __init__(self):
+        # Creating the ListStore model
+        self.ls_hist = Gtk.ListStore(str, str)
+        self.refresh_history()
+
+        self.tv_hist = Gtk.TreeView(model=self.ls_hist)
+        for i, column_title in enumerate(
+            ["Date", "QR Code"]
+        ):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column_title, renderer, text=i)
+            self.tv_hist.append_column(column)
+            if column_title == "QR Code":
+                column.set_resizable(True)
+                column.set_max_width(50)
+        select = self.tv_hist.get_selection()
+        select.set_mode(Gtk.SelectionMode.MULTIPLE)
+        select.set_select_function(self.selected_hist_entry)
+
+        # setting up the layout, putting the treeview in a scrollwindow, and the buttons in a row
+        stv_hist = Gtk.ScrolledWindow()
+        stv_hist.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        stv_hist.add(self.tv_hist)
+        self.box.pack_start(stv_hist, True, True, 1)
+
+    def refresh_history(self):
+        self.disable_actions=True
+        self.hist_codes=qr_saved.get_history()
+        for row in self.hist_codes:
+            self.ls_hist.append(row)
+        self.disable_actions=False
+
+    def selected_hist_entry(self,null1, null2, null3, null4):
+        if self.disable_actions:
+            return
+        path,data = self.tv_hist.get_cursor()
+        if path == None:
+            return
+        indices = path.get_indices()
+        
+        
+        qr_code=self.hist_codes[indices[0]][1]
+        
+        gui_process.qr_gui_handle_code(qr_code)
+        return False
+
+    def get_box(self):
+        return self.box
+
+
+        
+def qr_gui_main():
+    win = QrMainWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
--- src/lib/qr_alchemy/gui_main.py
+++ src/lib/qr_alchemy/gui_main.py
@@ -1,11 +1,8 @@
 
 import gi
-import qr_alchemy.process as qr_process
-import qr_alchemy.saved as qr_saved
-import qr_alchemy.gui as gui
-import qr_alchemy.gui_process as gui_process
 import qr_alchemy.gui_config as gui_config
 import qr_alchemy.gui_saved as gui_saved
+import qr_alchemy.gui_hist as gui_hist
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk,Gio
@@ -32,15 +29,7 @@ class QrMainWindow(Gtk.Window):
         super().__init__(title="QR Alchemy")
         self.set_border_width(0)
 
-        self.notebook = Gtk.Notebook()
-        self.add(self.notebook)
-        page_saved = gui_saved.QrSavedPage()
-        
-        
-        self.notebook.append_page(page_saved.get_box(), Gtk.Label('Saved'))
-        self.notebook.append_page(self.page_history(), Gtk.Label('History'))
-
-
+        # Title
         self.hb = Gtk.HeaderBar()
         self.hb.set_show_close_button(True)
         self.hb.props.title="QR Alchemy"
@@ -53,63 +42,22 @@ class QrMainWindow(Gtk.Window):
         bu_conf.connect("clicked", self.bu_config_clicked)
         self.hb.pack_end(bu_conf)
 
+        # Pages
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+        page_saved = gui_saved.QrSavedPage()
+        page_hist = gui_hist.QrHistPage()
+        
+        
+        self.notebook.append_page(page_saved.get_box(), Gtk.Label('Saved'))
+        self.notebook.append_page(page_hist.get_box(), Gtk.Label('History'))
+
         self.show_all()
 
     def bu_config_clicked(self, button):
         config_window = gui_config.qr_gui_config()
         self.first_select = 1
         
-
-    def page_history(self):
-        box = Gtk.Box()
-        # Creating the ListStore model
-        self.ls_hist = Gtk.ListStore(str, str)
-        self.refresh_history()
-
-        self.tv_hist = Gtk.TreeView(model=self.ls_hist)
-        for i, column_title in enumerate(
-            ["Date", "QR Code"]
-        ):
-            renderer = Gtk.CellRendererText()
-            column = Gtk.TreeViewColumn(column_title, renderer, text=i)
-            self.tv_hist.append_column(column)
-            if column_title == "QR Code":
-                column.set_resizable(True)
-                column.set_max_width(50)
-        select = self.tv_hist.get_selection()
-        select.set_mode(Gtk.SelectionMode.MULTIPLE)
-        select.set_select_function(self.selected_hist_entry)
-
-        # setting up the layout, putting the treeview in a scrollwindow, and the buttons in a row
-        stv_hist = Gtk.ScrolledWindow()
-        stv_hist.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        box.pack_start(stv_hist, True, True, 1)
-        stv_hist.add(self.tv_hist)
-        return box
-
-    def refresh_history(self):
-        self.disable_actions=True
-        self.hist_codes=qr_saved.get_history()
-        for row in self.hist_codes:
-            self.ls_hist.append(row)
-        self.disable_actions=False
-
-    def selected_hist_entry(self,null1, null2, null3, null4):
-        if self.disable_actions:
-            return
-        #if self.hist_click == 0:
-        path,data = self.tv_hist.get_cursor()
-        if path == None:
-            return
-        indices = path.get_indices()
-        
-        
-        qr_code=self.hist_codes[indices[0]][1]
-        
-        gui_process.qr_gui_handle_code(qr_code)
-        return False
-
-
         
 def qr_gui_main():
     win = QrMainWindow()
