e0e6b741f279796eac66692c468fbd781723003a
steve <steve5289@users.noreply.github.com>
2021-09-19 12:30:14 -0600

    added ability to delete and get specific saved entries

--- src/lib/qr_alchemy/saved.py
+++ src/lib/qr_alchemy/saved.py
@@ -47,7 +47,28 @@ def set_saved_code(name,qr_code):
     pickle.dump(saved_codes, fh)
     fh.close()
 
-        
+def delete_saved_code(name):
+    savefile=_get_user_savefile()
+    saved_codes=get_saved_codes()
+    fh = open(savefile, 'wb')
+    
+    saved_codes.pop(name)
+    pickle.dump(saved_codes, fh)
+    fh.close()
+
+def is_code_saved(qr_code):
+    saved_codes=get_saved_codes()
+    if qr_code in saved_codes.values():
+        return True
+    return False
+
+def get_code_saved_name(qr_code):
+    saved_codes=get_saved_codes()
+    for key in saved_codes.keys():
+        if qr_code == saved_codes[key]:
+            return key
+    return None
+
 def get_history():
     histfile=_get_user_histfile()
     codes = list()
