2ba34905d8c7d900ad200136fa8c65de35c41f21
steve <steve5289@users.noreply.github.com>
2021-09-24 11:42:14 -0600

    make a temp file for each qr code written

--- src/lib/qr_alchemy/generate.py
+++ src/lib/qr_alchemy/generate.py
@@ -4,11 +4,19 @@ import subprocess
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk,Gio,Gdk, GdkPixbuf
 import qr_alchemy.gui as gui
+import tempfile
 
 def generate_qr_img(qr_code):
     prog = 'qrencode'
-    tmpfile="/tmp/qr_code.png"
+    
+    fh = tempfile.NamedTemporaryFile()
+    tmpfile=fh.name
+
     args = [ '-o', tmpfile,'-s',100, qr_code]
     cmd=subprocess.run([prog, '-o', tmpfile, qr_code])
 
-    return gui.ResizableImage(path=tmpfile)
+    print('tmpfile:', tmpfile)
+
+    image_gui = gui.ResizableImage(path=tmpfile)
+    fh.close()
+    return image_gui
