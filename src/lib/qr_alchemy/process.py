d7c61baca4f4a89dd4b95dafc88caf47d1edc636
steve <steve5289@users.noreply.github.com>
2021-09-27 17:37:24 -0600

    fixed a bug where if the system default wasn't defined this failed

--- src/lib/qr_alchemy/process.py
+++ src/lib/qr_alchemy/process.py
@@ -33,7 +33,7 @@ def qr_exec(action, qr_code):
 
     if a_type == "System Default":
         subprocess.run(['xdg-open', qr_code])
-    if a_type == "Program":
+    elif a_type == "Program":
         subprocess.run([a_subtype, qr_code])
     elif a_type == 'Plugin':
         qr_plugins.run_input_plugin(a_subtype,qr_code)
@@ -46,10 +46,14 @@ def qr_get_header(qr_code):
     
 def qr_get_action(header):
     header2action = qr_code2action()
+    print('header2action:', header2action)
 
     if header in header2action:
         return header2action[header]
-    return header2action['*']
+    elif '*' in header2action:
+        return header2action['*']
+    else:
+        return ['System Default']
 
 
 def qr_code2action():
